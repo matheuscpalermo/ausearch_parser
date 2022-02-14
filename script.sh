4f0ec8ad1e050cc85da6a146bcfca70d73f9fffd
matheus <mathe@DESKTOP-VOMTHO4.localdomain>
2022-02-14 12:37:10 -0300

    debugging

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ausearch -i --checkpoint ./log_files/checkpoint > ausearch-i.txt;
+ausearch -i --checkpoint ./log_files/checkpoint > ./log_files/ausearch-i.txt;
 
 python3 ./ausearch_parser.py > /var/log/secure/auditd_logs.json
