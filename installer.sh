d6cbb96fb5ef65f9805bd236624a855fa1aab596
matheuscpalermo <45541838+matheuscpalermo@users.noreply.github.com>
2022-02-14 16:18:12 -0300

    update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,13 +6,13 @@ sudo chmod 750 /usr/local/bin/ausearch_parser/script.sh
 sudo chmod 750 /usr/local/bin/ausearch_parser/ausearch_parser.py
 
 ## install auditd
-sudo apt install auditd
+sudo apt install auditd -y
 
 ## start auditd
 sudo service auditd start
 
 ## enable auditd
-sudo systemctl --now enable suditd
+sudo systemctl --now enable auditd
 
 ## grub configuration
 sudo echo 'GRUB_CMDLINE_LINUX="audit=1"' >> /etc/default/grub
@@ -21,7 +21,7 @@ sudo update-grub
 
 ## replace auditd.conf
 sudo mv /etc/audit/auditd.conf /etc/audit/original_conf.txt
-sudo mv /usr/local/bin/ausearch_parser/auditd.conf mv /etc/audit/auditd.conf
+sudo mv /usr/local/bin/ausearch_parser/auditd.conf /etc/audit/auditd.conf
 
 ## replace auditd.rules
 sudo mv /etc/audit/rules.d/audit.rules /etc/audit/rules.d/original_rules.txt
@@ -29,9 +29,6 @@ sudo mv /usr/local/bin/ausearch_parser/audit.rules /etc/audit/rules.d/audit.rule
 
 ## create config file
 echo "LOGS_PATH = './log_files/ausearch-i.txt'" > /usr/local/bin/ausearch_parser/config.py
-
-## enabling cron
-sudo systemctl --now enable cron
 
 ## scheduling script
 sudo echo '15 * * * * root /usr/local/bin/ausearch_parser/script.sh' > /etc/crontab
