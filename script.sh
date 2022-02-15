a3cb64fed6f7955f6506587ca5561c3c3354ec8e
matheuscpalermo <45541838+matheuscpalermo@users.noreply.github.com>
2022-02-14 22:48:26 -0300

    update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ausearch -i --checkpoint /usr/local/bin/ausearch_parser/log_file/checkpoint > /usr/local/bin/ausearch_parser/log_file/ausearch-i.txt;
+sudo ausearch -i --checkpoint /usr/local/bin/ausearch_parser/log_file/checkpoint > /usr/local/bin/ausearch_parser/log_file/ausearch-i.txt;
 
-python3 /usr/local/bin/ausearch_parser/ausearch_parser.py > /var/log/auditd.json
+sudo python3 /usr/local/bin/ausearch_parser/ausearch_parser.py > /var/log/auditd.json
